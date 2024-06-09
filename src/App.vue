861dc14c7c56222bc6dfd1295cad6f241b8babb8
AlexTolk <alexandr.tolkachyov@gmail.com>
2024-06-09 23:17:16 +0800

    added the app functionality

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,117 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  import axios from 'axios'
+  export default {
+    data() {
+      return {
+        city: "",
+        error: "",
+        info: null
+      }
+    },
+    computed: {
+      showTemp() {
+        return "Temperature: " + this.info.main.temp
+      },
+      showFeelsLike() {
+        return "Feels like: " + this.info.main.feels_like
+      },
+      showMinTemp() {
+        return "Minimal temperature today: " + this.info.main.temp_min
+      },
+      showMaxTemp() {
+        return "Maximum temperature today: " + this.info.main.temp_max
+      }
+    },
+    methods: {
+      getWeather() {
+        if(this.city.trim().length < 2){
+          this.error = "At least one symbol"
+          return false
+        }
+
+        this.error = ""
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=babcf2f5a48399ca552505e591826f0e`)
+          .then((res) => this.info = res.data)
+      }
+    }
+  }
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <div class="wrapper">
+      <h1>Weather APP</h1>
+      <p>What is the weather like in {{ city=="" ? "your city " : city }}?</p>
+      <input type="text" v-model="city" placeholder="City">
+      <button v-if="city !== ''" @click="getWeather()">See the weather</button>
+      <button disabled v-else>Which city?</button>
+      <p class="error">{{ error }}</p>
+
+      <div v-if="info !== null">
+        <p>{{ showTemp }}</p>
+        <p>{{ showFeelsLike }}</p>
+        <p>{{ showMinTemp }}</p>
+        <p>{{ showMaxTemp }}</p>
+      </div>
+    </div>
+
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .wrapper {
+    width: 900px;
+    height: 500px;
+    border-radius: 50px;
+    padding: 20px;
+    background: #383131;
+    text-align: center;
+    color: #fff;
+  }
+  .wrapper h1 {
+    margin-top: 50px;
+  }
+  .wrapper p {
+    margin-top: 20px;
+  }
+  .wrapper input {
+    margin-top: 30px;
+    background-color: transparent;
+    border: 0;
+    border-bottom:2px solid #0e0e0e;
+    color: #fff;
+    font-size: 14px;
+    padding: 5px 8px;
+    outline: none;
+  }
+  .wrapper input:focus {
+    border-bottom-color: #6e2d7d;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  .wrapper button:disabled {
+    background-color: #9b43ae;
+    cursor: not-allowed;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .wrapper button {
+    background-color: #e33bc4;
+    color: #fff;
+    border-radius: 10px;
+    border: 2px solid #b999;
+    padding: 10px 15px;
+    margin-left: 20px;
+    cursor: pointer;
+    transition: transform 500ms ease;
   }
-}
+  .wrapper button:hover {
+    transform: scale(1.1), translateY(-5px)
+  }
+
+  .error {
+    color: rgb(185, 23, 23);
+  }
 </style>
